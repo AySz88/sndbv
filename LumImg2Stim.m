8bbe5ed3a58097ec74870c113e444ca9e4de5e1d
Alex Yuan <aysz88@gmail.com>
2013-12-20 16:31:20 -0500

    Stimulus generation from images: Split images into a grid instead of taking random crops

--- LumImg2Stim.m
+++ LumImg2Stim.m
@@ -1,4 +1,4 @@
-function [ ] = LumImg2Stim( path, w, h, scale, nPerImage )
+function [ ] = LumImg2Stim( path, w, h, xtiles, ytiles )
 %LUMIMG2STIM Creates stimuli from a folder of luminance matrices
 %   Detailed explanation goes here
 
@@ -25,26 +25,35 @@ for fileidx = 1:length(files)
     %srcImage = im2double(gpuArray(srcData.LUM_Image));
     srcImage = srcData.LUM_Image;
     
+    ymax = size(srcImage, 1);
+    xmax = size(srcImage, 2);
+    
+    % Pull a stimulus from as large an area of the image as possible
+    scale = max(w / (xmax/xtiles), h / (ymax / ytiles));
     scaledImage = imresize(srcImage, scale);
     
-    for rep = 1:nPerImage
-        ymax = size(scaledImage, 1);
-        xmax = size(scaledImage, 2);
-        
-        % Generate position of upper-left corner
-        xpos = ceil(rand()*(xmax - w));
-        ypos = ceil(rand()*(ymax - h));
-        
-        destImage = imcrop(scaledImage, [xpos, ypos, w, h]);
-        
-        % normalize to 0-1 and use full range
-        destImage = destImage - min(destImage(:));
-        destImage = destImage ./ max(destImage(:));
-        
-%         imshow(destImage);
-%         pause
-        destFileName = sprintf('%s%s%s stim %i.png', prepend, filesep, shortname, rep);
-        imwrite(destImage, destFileName);
+    ymax = size(scaledImage, 1);
+    xmax = size(scaledImage, 2);
+    
+    for xi = 1:xtiles
+        for yi = 1:ytiles
+            % Generate position of upper-left corner
+            xpos = ceil((xi-1)/xtiles*xmax)+1;
+            ypos = ceil((yi-1)/ytiles*ymax)+1;
+
+            destImage = imcrop(scaledImage, [xpos, ypos, w-1, h-1]);
+
+            % normalize to 0-1 and use full range
+            destImage = destImage - min(destImage(:));
+            destImage = destImage ./ max(destImage(:));
+
+%             fprintf('%s tile (%i %i): (%i, %i) ', shortname, xi, yi, xpos, ypos);
+%             fprintf('%ix%i (output %ix%i)\n', w, h, size(destImage, 1), size(destImage, 2));
+%             imshow(destImage);
+%             pause
+            destFileName = sprintf('%s%s%s stim %i.png', prepend, filesep, shortname, (xi-1)*ytiles + yi);
+            imwrite(destImage, destFileName);
+        end
     end
 end
 
